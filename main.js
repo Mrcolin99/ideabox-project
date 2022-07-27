389fd1eecb1006eb403d5872f11f72984ba6acee
Colin Ciervo <colinciervo@@gmail.com>
2022-07-27 13:36:17 -0400

    Feature: add save button functionality

--- main.js
+++ main.js
@@ -4,28 +4,35 @@ var starredIdeasButton = document.querySelector('.starredIdea')
 
 var titleInput = document.getElementById('idea-title')
 var bodyInput = document.getElementById('idea-body')
-var saveButton = document.querySelector('save-button')
+var saveButton = document.querySelector('.save-button')
 
-var searchIdeaBox = document.querySelector('align-search')
+var searchIdeaBox = document.querySelector('.align-search')
 
 
 
-var newIdea
+var currentTitle
+var currentBody
 //Arrays
-
 var ideaCards = []
-
 
 
 //Event listeners
 saveButton.addEventListener('click', saveCard)
+saveButton.addEventListener('click', clearInputs)
 
 
 
 
 //Functions
 function saveCard() {
-  var newIdea = new Idea(titleInput, bodyInput)
-  
-  event.preventDefault()
+ currentTitle = titleInput.value
+ currentBody = bodyInput.value
+ var newIdea = new Idea(currentTitle, currentBody)
+ event.preventDefault()
+ ideaCards.push(newIdea)
+}
+
+function clearInputs() {
+  titleInput.value = ''
+  bodyInput.value = ''
 }
